28ed0d1de3b47fe5b4d496099d9b1e82e44c2dcd
michael Means <colemeans51@gmail.com>
2022-01-12 14:52:17 -0600

    refractoring

--- _tests_/player.test.js
+++ _tests_/player.test.js
@@ -11,7 +11,21 @@ expect(player.name).toBe('Dave');
 expect(player.health).toEqual(expect.any(Number));
 expect(player.strength).toEqual(expect.any(Number));
 expect(player.agility).toEqual(expect.any(Number));
-expect(player.inventory).toEqual(
-    expect.arrayContaining([expect.any(Object)])
-);
-})
+expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
+});
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+    expect(player.getInventory()).toEqual(expect.any(Array));
+    player.inventory = [];
+    expect(player.getInventory()).toEqual(false);
+});
